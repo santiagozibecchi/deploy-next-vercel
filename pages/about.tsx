6fbd5b75f66c0046806dc1770eb9024480df83ac
Santiago Zibecchi <santiago.zibecchi@gmail.com>
2022-08-21 20:19:32 -0300

    migrado a TS

--- pages/about.tsx
+++ pages/about.tsx
@@ -19,7 +19,7 @@ export default function AboutPage() {
    );
 }
 
-AboutPage.getLayout = function getLayout(page) {
+AboutPage.getLayout = function getLayout(page: JSX.Element) {
    return (
       <MainLayout>
          <LightLayout>{page}</LightLayout>
